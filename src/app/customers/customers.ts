1a86a5949198631677b8acdea72516a372b4d3ec
layla <laylaelhajjaji@gmail.com>
2026-05-09 19:38:39 +0100

    updating customers components

--- src/app/customers/customers.ts
+++ src/app/customers/customers.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-customers',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './customers.html',
   styleUrl: './customers.css',
 })
-export class Customers {}
+export class Customers implements OnInit {
+  customers : any;
+  constructor(private http : HttpClient) {
+
+  }
+  ngOnInit() {
+    this.http.get('http://127.0.0.1:8085/customers').subscribe({
+      next  :(data) => this.customers = data,
+      error : (err)  => console.log(err)
+    })
+  }
+}
